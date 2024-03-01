0b3184fdf8f9f03a2afb31916db5c16ea3dc7592
brunonoatto <brunonoatto@gmail.com>
2024-03-01 15:20:58 -0300

    feat: add skills

--- src/app/(components)/skills/page.tsx
+++ src/app/(components)/skills/page.tsx
@@ -9,7 +9,10 @@ export default function Skills() {
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-12 text-center">
         <SkillItem src="/logos/html5.svg" alt="HTML5" />
         <SkillItem src="/logos/css3.svg" alt="CSS3" />
+        <SkillItem src="/logos/js.svg" alt="Javascript" />
+        <SkillItem src="/logos/ts.svg" alt="Typescript" />
         <SkillItem src="/logos/react.svg" alt="React" />
+        <SkillItem src="/logos/react-router.svg" alt="React Router" />
         <div className="flex flex-col justify-between">
           <div className="bg-white w-[110px] rounded-sm m-auto">
             <Image
@@ -22,16 +25,15 @@ export default function Skills() {
           </div>
           NextJs
         </div>
-        <SkillItem src="/logos/js.svg" alt="Javascript" />
-        <SkillItem src="/logos/ts.svg" alt="Typescript" />
         <SkillItem src="/logos/jest.svg" alt="Jest" />
         <SkillItem src="/logos/playwright.svg" alt="Playwright" />
         <SkillItem src="/logos/redux.svg" alt="Redux" />
         <SkillItem src="/logos/zustand.png" alt="Zustand" />
-        <SkillItem src="/logos/react-router.svg" alt="React Router" />
         <SkillItem src="/logos/react-query.svg" alt="React Query" />
+        <SkillItem src="/logos/react-hook-form.svg" alt="React Hook Form" />
         <SkillItem src="/logos/storybook.svg" alt="Storybook" />
         <SkillItem src="/logos/tailwind.svg" alt="Tailwind" />
+        <SkillItem src="/logos/sass.svg" alt="Sass" />
         <SkillItem src="/logos/git.svg" alt="Git" />
         <SkillItem src="/logos/github.svg" alt="GitHub" />
       </div>
