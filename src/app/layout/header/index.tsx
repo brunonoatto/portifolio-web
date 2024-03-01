c3d94610584290a2e363a0aebcf5a324483d8432
brunonoatto <brunonoatto@gmail.com>
2024-03-01 03:18:03 -0300

    feat: adicionado nome completo no header

--- src/app/layout/header/index.tsx
+++ src/app/layout/header/index.tsx
@@ -3,7 +3,10 @@ import Menu from "./menu";
 export default function Header() {
   return (
     <header className=" sticky top-0 flex items-center justify-between p-4 rounded-lg shadow-md shadow-red-500">
-      <div className="text-2xl font-bold">Portifólio</div>
+      <div>
+        <div className="text-xl font-bold">Portifólio</div>
+        <div>Bruno Noatto Iserhardt</div>
+      </div>
       <Menu />
     </header>
   );
