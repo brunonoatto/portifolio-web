0d5333317a826f2d04a4f0d40e16ca1c81268d4b
brunonoatto <brunonoatto@gmail.com>
2024-03-21 22:26:14 -0300

    feat: renomeada a pasta components para pages

---
+++ src/app/(pages)/skills/page.tsx
@@ -0,0 +1,11 @@
+import HardSkills from "@/app/(pages)/skills/hard-skills";
+import SoftSkills from "@/app/(pages)/skills/soft-skills";
+
+export default function Skills() {
+  return (
+    <>
+      <SoftSkills />
+      <HardSkills />
+    </>
+  );
+}
